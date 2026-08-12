817a93de73e2cafc5984efe7f4a7d07a08343102
Stanislav Emelyanov <stasolet@gmail.com>
2026-08-13 01:46:42 +0300

    Базовая настройка и mock для чата

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import chatsMock from './src/mocks/chats.json' assert { type: 'json' };
+
+export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: './src/conponents',
+      context: {
+        siteTitle: 'Мой проект',
+        chats: chatsMock,
+      },
+    }),
+  ],
+  // Корень для сборки (обычно src)
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'src/pages/index/index.hbs',
+      },
+    },
+  },
+});
