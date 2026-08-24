aef096d4cad5755281e9e4b2099269c5eeed2084
Stanislav Emelyanov <s.emelyanov@inforion.ru>
2026-08-24 16:08:38 +0300

    add errors

---
+++ src/pages/error_404/error_404.js
@@ -0,0 +1,12 @@
+import Handlebars from "handlebars";
+import errorTpl from "../../layouts/error/error.hbs?raw";
+
+import "../../layouts/error/error.css";
+import "../../index.css"
+const templateContent = {
+  code: "404",
+  message: "Не туда попали",
+};
+
+const compileError = Handlebars.compile(errorTpl);
+document.getElementById("error-container").innerHTML = compileError(templateContent);
