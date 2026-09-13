9737b48048ae79af8cf05e59d5ebf9b783c78f06
Stanislav Emelyanov <stasolet@gmail.com>
2026-09-13 23:39:16 +0300

    Добавлен вызон unmount

--- src/shared/ui/base/base.ts
+++ src/shared/ui/base/base.ts
@@ -79,6 +79,7 @@ export abstract class BaseBlock<
   protected render() {
     const fragment = this.compile();
     if (this.domElement && fragment) {
+      this.unmountComponent();
       this.domElement.replaceWith(fragment);
     }
 
